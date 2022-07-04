71bfaece8ab0200c2565f9c2a1101cf661e4a41b
Stoilovskyi Vadym <vadim.stojlovskij@murka.com>
2022-07-04 15:43:47 +0300

    [+] Add hook generating new test run description

--- pytest_testrail_integrator/new_hooks.py
+++ pytest_testrail_integrator/new_hooks.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.hookspec(firstresult=True)
 def pytest_tr_generate_run_name(config) -> str:
     """Called for generating new test run name. Takes first hook result."""
+
+
+@pytest.hookspec(firstresult=True)
+def pytest_tr_generate_run_description(config) -> str:
+    """Called for generating new test run description. Takes first hook result."""
